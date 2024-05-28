ae299eac4389d037bd09d86f466b53d25fec1465
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-28 17:34:24 +0200

    ForEach in classe table.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,40 @@
     <title>Document</title>
 </head>
 <body>
+    
+    <div class="container">
 
-    <h1 class="text-center">CIAO</h1>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Slug</th>
+      <th scope="col">Type</th>
+      <th scope="col">Category</th>
+      <th scope="col">Weight</th>
+      <th scope="col">Cost</th>
+      <th scope="col">Damage Dice</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($items as $item)
+    <tr>
+      <th scope="row">{{$item->id }}</th>
+      <td>{{ $item->name }}</td>
+      <td>{{ $item->slug }}</td>
+      <td>{{ $item->type }}</td>
+      <td>{{ $item->category }}</td>
+      <td> {{ $item->weight }}</td>
+      <td>{{ $item->cost }}</td>
+      <td> {{$item->damage_dice }}</td>
+    </tr>     
+    @endforeach
+  </tbody>
+</table>
+
+    </div>
     
 </body>
 </html>
