026ecdcb904c0700b3eea7e1ccde48e27d18dee4
corgab <corbanigabriele@gmail.com>
2024-05-31 17:33:48 +0200

    ADD: style

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -3,52 +3,58 @@
 @section('title','Characters')
 
 @section('content')
-<div class="container">
+<div class="bg-body-secondary py-5" style="height: 100vh;">
+  <div class="container">
+    <h1 class="text-center fs-2 mb-5">CHARACTERS</h1>
     <table class="table">
-        <thead>
+      <thead>
+        <tr>
+          <th scope="col" class="text-black-50">Name</th>
+          {{-- <th scope="col">Description</th> --}}
+          <th scope="col" class="text-black-50">Atk</th>
+          <th scope="col" class="text-black-50">Def</th>
+          <th scope="col" class="text-black-50">Speed</th>
+          <th scope="col" class="text-black-50">HP</th>
+          <th scope="col" class="text-black-50">More</th>
+          <th scope="col" class="text-black-50">Edit</th>
+          <th scope="col" class="text-black-50">Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+          @foreach ($characters as $character)
           <tr>
-            <th scope="col">Name</th>
-            {{-- <th scope="col">Description</th> --}}
-            <th scope="col">Atk</th>
-            <th scope="col">Def</th>
-            <th scope="col">Speed</th>
-            <th scope="col">HP</th>
-            <th scope="col">More</th>
-            <th scope="col">Edit</th>
-            <th scope="col">Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-            @foreach ($characters as $character)
-            <tr>
-                <th scope="row">{{ $character->name }}</th>
-                {{-- <td>{{ $character->description }}</td> --}}
-                <td>{{ $character->attack }}</td>
-                <td>{{ $character->defence }}</td>
-                <td>{{ $character->speed }}</td>
-                <td>{{ $character->life }}</td>
-                <td>
-                  <a href="{{route('characters.show', $character)}}" class="btn btn-primary">Detail</a>
-                </td>
-                <td>
-                  <a href="{{route('characters.edit', $character)}}" class="btn btn-warning">Edit</a>
-                </td>
-                <td>
-                  <form action="{{route('characters.destroy', $character)}}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-danger">Delete</button>
+              <th scope="row">
+                <a href="{{route('characters.show', $character)}}">{{ $character->name }}</a>
+                </th>
+              {{-- <td>{{ $character->description }}</td> --}}
+              <td>{{ $character->attack }}</td>
+              <td>{{ $character->defence }}</td>
+              <td>{{ $character->speed }}</td>
+              <td>{{ $character->life }}</td>
+              <td>
+                <a href="{{route('characters.show', $character)}}" class="btn btn-primary">Detail</a>
+              </td>
+              <td>
+                <a href="{{route('characters.edit', $character)}}" class="btn btn-warning">Edit</a>
+              </td>
+              <td>
+                <form action="{{route('characters.destroy', $character)}}" method="POST">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger">Delete</button>
 
-                  </form>
-                </td>
-            </tr>
-          @endforeach
-        </tbody>
-      </table>
-        <div>
-          <a href="{{route('characters.create')}}" class="btn btn-success">Create new Character</a>
-        </div>
+                </form>
+              </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+    <div>
+      <a href="{{route('characters.create')}}" class="btn btn-success">Create new Character</a>
+    </div>
+  </div>
 </div>
+
 
 
 @endsection
--- resources/views/characters/show.blade.php
+++ resources/views/characters/show.blade.php
@@ -4,30 +4,34 @@
 
 @section('content')
 {{-- <div class="container"> --}}
-    <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
-        <div class="text-center">
-            <div>
-                <h1>Nome: {{$character->name}}</h1>
-                <p>{{$character->description}}</p>
-            </div>
-            <div>
-                <ul class="p-0">
-                    <li>Atk: {{$character->attack}}</li>
-                    <li>Def: {{$character->defence}}</li>
-                    <li>Speed: {{$character->speed}}</li>
-                    <li>HP: {{$character->life}}</li>
-                </ul>
-                <div class="d-flex justify-content-center gap-5">
-                    <a href="{{route('characters.index')}}" class="btn btn-primary">Lista</a>
-                    <form action="{{route('characters.destroy', $character)}}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger">Delete</button>
-                      </form>
+    <div class="bg-dark-subtle">
+        <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
+            <div class="text-center">
+                <div>
+                    <h1>Nome: {{$character->name}}</h1>
+                    <p>{{$character->description}}</p>
+                </div>
+                <div>
+                    <ul class="p-0">
+                        <li>Atk: {{$character->attack}}</li>
+                        <li>Def: {{$character->defence}}</li>
+                        <li>Speed: {{$character->speed}}</li>
+                        <li>HP: {{$character->life}}</li>
+                    </ul>
+                    <div class="d-flex justify-content-center gap-5">
+                        <a href="{{route('characters.index')}}" class="btn btn-success btn-lg ">Lista</a>
+                        <a href="{{route('characters.edit', $character)}}" class="btn btn-light btn-lg ">Edit</a>
+                        <form action="{{route('characters.destroy', $character)}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger btn-lg ">Delete</button>
+                          </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
 
 
 {{-- </div> --}}
