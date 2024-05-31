9667b06129a47622527dd79104e2d8d841604b53
corgab <corbanigabriele@gmail.com>
2024-05-31 15:38:57 +0200

    ADD: characters migrations + model

---
+++ database/migrations/2024_05_31_133305_create_characters_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 200);
+            $table->text('description');
+            $table->integer('attack');
+            $table->integer('defence');
+            $table->integer('speed');
+            $table->integer('life');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('characters');
+    }
+};
