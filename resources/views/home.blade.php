637541fd75851d533efc7ebcd0a10961b484b8a5
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-06-04 15:57:37 +0200

    ADD: auth configuration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,38 +4,36 @@
 
 @section('content')
 
-  <div class="container">
-    <h1 class="text-center mb-5">Game Creator by Boolean's Team 3</h1>
-    <table class="table">
-      <thead>
-        <tr>
-          <th class="text-danger" scope="col">ID</th>
-          <th class="text-danger" scope="col">Name</th>
-          <th class="text-danger" scope="col">Slug</th>
-          <th class="text-danger" scope="col">Type</th>
-          <th class="text-danger" scope="col">Category</th>
-          <th class="text-danger" scope="col">Weight</th>
-          <th class="text-danger" scope="col">Cost</th>
-          <th class="text-danger" scope="col">Damage Dice</th>
-
-        </tr>
-      </thead>
-      <tbody>
-        @foreach ($items as $item)
+<div class="container">
+  <h1 class="text-center mb-5">Game Creator by Boolean's Team 3</h1>
+  <table class="table">
+    <thead>
       <tr>
-      <th scope="row">{{ $item->id }}</th>
-      <td>{{ $item->name }}</td>
-      <td>{{ $item->slug }}</td>
-      <td>{{ $item->type }}</td>
-      <td>{{ $item->category }}</td>
-      <td>{{ $item->weight }}</td>
-      <td>{{ $item->cost }}</td>
-      <td>{{ $item->damage_dice }}</td>
+        <th class="text-danger" scope="col">ID</th>
+        <th class="text-danger" scope="col">Name</th>
+        <th class="text-danger" scope="col">Slug</th>
+        <th class="text-danger" scope="col">Type</th>
+        <th class="text-danger" scope="col">Category</th>
+        <th class="text-danger" scope="col">Weight</th>
+        <th class="text-danger" scope="col">Cost</th>
+        <th class="text-danger" scope="col">Damage Dice</th>
       </tr>
-    @endforeach
-      </tbody>
-    </table>
-
-  </div>
+    </thead>
+    <tbody>
+      @foreach ($items as $item)
+      <tr>
+        <th scope="row">{{ $item->id }}</th>
+        <td>{{ $item->name }}</td>
+        <td>{{ $item->slug }}</td>
+        <td>{{ $item->type }}</td>
+        <td>{{ $item->category }}</td>
+        <td>{{ $item->weight }}</td>
+        <td>{{ $item->cost }}</td>
+        <td>{{ $item->damage_dice }}</td>
+      </tr>
+      @endforeach
+  </tbody>
+  </table>
+</div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
-
 use App\Http\Controllers\CharacterController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +16,23 @@ use App\Http\Controllers\CharacterController;
 |
 */
 
-Route::get('/', [ItemController::class, 'home'])->name('home');
+Route::middleware('auth')->group(function () {
 
-Route::resource('characters', CharacterController::class);
+    Route::get('/', [ItemController::class, 'home'])->name('home');
+
+    Route::resource('characters', CharacterController::class);
+
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+require __DIR__.'/auth.php';
