c59e428de511d031a715dcb7b199ef4b6b2728e2
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-06-18 16:38:36 +0200

    Refactor: general

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    {{-- Try Branch Rachel --}}
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title')</title>
-
-    <h1>Test</h1>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
